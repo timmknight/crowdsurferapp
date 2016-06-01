32a1f6aefd6d4b198c5fa27ca32040507c1884f2
Tim Knight <timmilesknight@gmail.com>
2016-06-01 21:09:04 +0100

    pagination via bootstrap, page size filter & limitto

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,42 +8,40 @@
  * Controller of the crowdSurferApp
  */
 angular.module('crowdSurferApp')
-  .controller('MainCtrl', ['$scope', '$http', 'projectDataService', function ($scope, $http, projectDataService) {
-    $scope.filteredData = []
-    $scope.currentPage = 0;
-    $scope.maxSize = 5;
+  .controller('MainCtrl', ['$scope', '$http', '$filter', 'projectDataService', function ($scope, $http, $filter, projectDataService) {
+    $scope.filteredData = [];
+    $scope.currentPage = 1;
     $scope.pageSize = 10;
-    $scope.pages = [];
     $scope.numberOfPages;
     
+    $scope.pageSizeOptions = [3,5,10,20];
+    
+    $scope.setItemsPerPage = function(num) {
+      $scope.pageSize = num;
+      $scope.currentPage = 1;
+    }
 
+    $scope.setPage = function (pageNo) {
+      $scope.currentPage = pageNo;
+    };
+    
     projectDataService.get().then(function (response) {
-      
+
       $scope.data = response.data;
       $scope.totalItems = response.data.length;
       $scope.numberOfPages = Math.ceil($scope.data.length / $scope.pageSize);
 
       for (var i = 0; i < response.data.length; i++) {
-        console.log(response.data[i]);
-        $scope.filteredData.push(response.data[i]);        
+        $scope.filteredData.push(response.data[i]);
         // mutate filteredData rather than the response
-        $scope.filteredData[i].percentage = Math.round((($scope.filteredData[i].raised / $scope.filteredData[i].goal) * 100));     
+        $scope.filteredData[i].percentage = Math.round((($scope.filteredData[i].raised / $scope.filteredData[i].goal) * 100));
       }
       
-      for (var n = 0; n <= $scope.numberOfPages - 1; n++) {
-        $scope.pages.push(n);
-      }
     });
   }])
 
-  .filter('startFrom', function () {
-    return function (input, start) {
-      start = +start; //parse to int
-      return input.slice(start);
-    }
-  })
 
-  .directive("projectSummary", function () {  
+  .directive("projectSummary", function () {
     return {
       restrict: 'ACEM',
       templateUrl: '../../views/directives/projectSummary.html',
@@ -52,6 +50,17 @@ angular.module('crowdSurferApp')
       }
     }
   })
+  
+  // .directive("projectList", function () {
+  //   return {
+  //     restrict: 'ACEM',
+  //     templateUrl: '../../views/directives/projectList.html'
+  //     ,
+  //     scope: {
+  //       inputData: "="
+  //     }
+  //   }
+  // })
 
   .service('projectDataService', ['$http', function ($http) {
     var dataUrl = 'https://s3-eu-west-1.amazonaws.com/crowdsurfer-json-dumps/blockchain-projects.json';
@@ -61,4 +70,4 @@ angular.module('crowdSurferApp')
       }
     }
   }
-]);
+  ]);
