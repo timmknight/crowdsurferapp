ebe03f32e255a158eb414fbc7e9616411ea2c026
Tim Knight <timmilesknight@gmail.com>
2016-06-05 13:44:42 +0100

    Funding type filter, tidy up a little

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name crowdSurferApp.controller:MainCtrl
@@ -7,94 +6,92 @@
  * # MainCtrl
  * Controller of the crowdSurferApp
  */
-angular.module('crowdSurferApp')
-  .controller('MainCtrl', ['$scope', '$http', '$filter', 'projectDataService', 'fundingTypeFilter', function ($scope, $http, $filter, projectDataService, fundingTypeFilter) {
-    $scope.filteredData = [];
-    $scope.currentPage = 1;
-    $scope.pageSize = 10;
-    $scope.numberOfPages;
-
-    $scope.pageSizeOptions = [3, 5, 10, 20];
-
-    $scope.setItemsPerPage = function (num) {
-      $scope.pageSize = num;
-      $scope.currentPage = 1;
+angular.module('crowdSurferApp').controller('MainCtrl', ['$scope', '$http', '$filter', 'projectDataService',
+    function($scope, $http, $filter, projectDataService) {
+        // Set up scope
+        $scope.filteredData = [];
+        $scope.currentPage = 1;
+        $scope.pageSize = 10;
+        $scope.pageSizeOptions = [3, 5, 10, 20];
+        // For filtering by§ funding type
+        $scope.fundingTypes = {
+            equity: true,
+            reward: true
+        };
+        projectDataService.get().then(function(response) {
+            $scope.filteredData = response.data;
+            var calcPercentage = function(project) {
+                // ensure goal & raise are not 0, if they are set to 0 to avoid division issues
+                if (project.goal > 0 && project.raised > 0) {
+                    var divide = _.divide(project.raised, project.goal);
+                    var percentage = _.multiply(divide, 100);
+                    project.percentage = _.round(percentage, 2);
+                } else {
+                    project.percentage = 0;
+                }
+            };
+            _.map($scope.filteredData, calcPercentage);
+        });
     }
-
-    $scope.setPage = function (pageNo) {
-      $scope.currentPage = pageNo;
+]).filter('readableFundingType', function() {
+    return function(projects) {
+        var readableFundingTypeData = [];
+        _.map(projects, function(project) {
+            if (project.funding_type === 'R') {
+                project.funding_type = 'Reward';
+            } else if (project.funding_type === 'E') {
+                project.funding_type = 'Equity';
+            }
+            readableFundingTypeData.push(project);
+        });
+        return readableFundingTypeData;
     };
-
-    projectDataService.get().then(function (response) {
-
-      $scope.filteredData = response.data;
-      $scope.totalItems = response.data.length;
-      $scope.numberOfPages = _.ceil($scope.filteredData.length / $scope.pageSize);
-
-      var calcPercentage = function (project) {
-        if (project.goal > 0 && project.raised > 0) {
-          var divide = _.divide(project.raised, project.goal);
-          var percentage = _.multiply(divide, 100);
-          project.percentage = _.round(percentage, 2);
-        } else {
-          project.percentage = 0;
-        }
-      };
-      
-      _.map($scope.filteredData, calcPercentage);
-      
-      var unifyFundingType =function(project){
-        if (project.funding_type) {
-          project.funding_type = fundingTypeFilter(project.funding_type);
-        }
-      }
-
-      _.map($scope.filteredData, unifyFundingType);
-      
-    });
-  }])
-
-  .filter('fundingType', function () {
-    return function (input) {
-      if (input === 'R') {
-        return 'Reward'
-      } else if (input === 'E') {
-        return 'Equity'
-      }
+}).filter('test', function() {
+    return function(projects, fundingType) {
+        var fundingTypeFilteredData = [];
+        _.map(projects, function(project) {
+            // Not clean - there is probably a much cleaner way to do this 
+            if (fundingType.equity === true && fundingType.reward === true) {
+                fundingTypeFilteredData.push(project);
+            } else if (fundingType.equity === true && fundingType.reward === false) {
+                if (project.funding_type === 'Equity') {
+                    fundingTypeFilteredData.push(project);
+                }
+            } else if (fundingType.equity === false && fundingType.reward === true) {
+                if (project.funding_type === 'Reward') {
+                    fundingTypeFilteredData.push(project);
+                }
+            }
+        });
+        return fundingTypeFilteredData;
     };
-  })
-
-  .directive("projectList", function () {
+}).directive("projectList", function() {
     return {
-      restrict: 'AE',
-      templateUrl: '../../views/directives/projectList.html',
-      scope: {
-        data: "=",
-        pageSize: "=",
-        currentPage: "=",
-        numberOfPages: "=",
-        pageSizeOptions: "=",
-        totalItems: "="
-      }
-    }
-  })
-
-  .directive("projectSummary", function () {
+        restrict: 'AE',
+        templateUrl: '../../views/directives/projectList.html',
+        scope: {
+            data: "=",
+            pageSize: "=",
+            currentPage: "=",
+            pageSizeOptions: "=",
+            fundingTypes: "="
+        }
+    };
+}).directive("projectSummary", function() {
     return {
-      restrict: 'AE',
-      templateUrl: '../../views/directives/projectSummary.html',
-      scope: {
-        project: "="
-      }
+        restrict: 'AE',
+        templateUrl: '../../views/directives/projectSummary.html',
+        scope: {
+            project: "="
+        }
+    };
+}).service('projectDataService', ['$http',
+    function($http) {
+        var dataUrl = 'https://s3-eu-west-1.amazonaws.com/crowdsurfer-json-dumps/blockchain-projects.json';
+        return {
+            get: function() {
+                return $http.get(dataUrl);
+            }
+        };
     }
-  })
-
-  .service('projectDataService', ['$http', function ($http) {
-    var dataUrl = 'https://s3-eu-west-1.amazonaws.com/crowdsurfer-json-dumps/blockchain-projects.json';
-    return {
-      get: function () {
-        return $http.get(dataUrl);
-      }
-    }
-  }
-  ]);
+]);
